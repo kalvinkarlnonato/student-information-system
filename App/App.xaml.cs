fce89e21c9a3e104b303ae09733417d1619c7c8d
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2024-01-23 08:59:45 +0800

    Add project files.

---
+++ App/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace App;
+
+public partial class App : Application
+{
+    private Window? MainWindow;
+
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        MainWindow = new MainWindow();
+        MainWindow.Activate();
+    }
+
+}
---
+++ App/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using WinUIEx;
+
+namespace App;
+
+public sealed partial class MainWindow : WindowEx
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
