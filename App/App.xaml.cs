971d6eb4fe839a31504f0acffa7d4dbcb05a94a4
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2024-01-23 09:37:11 +0800

    Added dependency injection

--- App/App.xaml.cs
+++ App/App.xaml.cs
@@ -1,20 +1,32 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Xaml;
+using WinUIEx;
 
 namespace App;
 
 public partial class App : Application
 {
-    private Window? MainWindow;
+
+    public static IHost? Hosting { get; private set; }
 
     public App()
     {
         this.InitializeComponent();
+
+        Hosting = Host.CreateDefaultBuilder().
+            UseContentRoot(AppContext.BaseDirectory).
+            ConfigureServices((context, services) => {
+                services.AddTransient<MainWindow>();
+            }).Build();
     }
 
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
     {
-        MainWindow = new MainWindow();
-        MainWindow.Activate();
+        base.OnLaunched(args);
+        await Hosting!.StartAsync();
+        var MainWindow = Hosting.Services.GetRequiredService<MainWindow>();
+        MainWindow.Show();
     }
 
 }
--- App/MainWindow.xaml.cs
+++ App/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Xaml;
 using WinUIEx;
 
 namespace App;
@@ -7,5 +8,10 @@ public sealed partial class MainWindow : WindowEx
     public MainWindow()
     {
         this.InitializeComponent();
+    }
+
+    private async void WindowEx_Closed(object sender, WindowEventArgs args)
+    {
+        await App.Hosting!.StopAsync();
     }
 }
